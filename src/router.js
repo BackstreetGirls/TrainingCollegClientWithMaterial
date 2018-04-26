2a1f3fa25f242da5405ae97ec25dda34d7a6bd82
sherley_huang <834998556@qq.com>
2018-04-26 17:12:09 +0800

    简单写了一下首页

--- src/router.js
+++ src/router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
-import IndexPage from './routes/IndexPage';
+import HomePage from './routes/HomePage';
+// import IndexPage from './routes/IndexPage';
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
-        <Route path="/" exact component={IndexPage} />
+        <Route path="/" exact component={HomePage} />
+        <Route path="/homepage" exact component={HomePage}/>
       </Switch>
     </Router>
   );
---
+++ src/routes/HomePage.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import { Carousel } from 'antd';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import SwipeableDrawer from 'material-ui/SwipeableDrawer';
+
+import './css/HomePage.css';
+
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flex: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: 'auto',
+  },
+};
+
+class HomePage extends React.Component {
+
+  state = {
+    open_drawer: false,
+  };
+
+  toggleDrawer = (state) => () => {
+    this.setState({
+      open_drawer: state,
+    });
+  };
+
+  render() {
+
+    const { classes } = this.props;
+    const iOS = process.browser && /iPad|iPhone|iPod/.test(navigator.userAgent);
+
+    const sideList = (
+      <div className={classes.list}>
+        // todo
+      </div>
+    );
+
+    return (
+      <div>
+        <AppBar position="static" color="white">
+          <Toolbar>
+            <IconButton
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="Menu"
+              onClick={this.toggleDrawer(true)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="title" color="inherit" className={classes.flex}>
+              TrainingCollege
+            </Typography>
+            <Button color="inherit">Login</Button>
+            <Button color="inherit">Register</Button>
+          </Toolbar>
+        </AppBar>
+
+        <Carousel autoplay>
+          <div><h3>1</h3></div>
+          <div><h3>2</h3></div>
+          <div><h3>3</h3></div>
+          <div><h3>4</h3></div>
+        </Carousel>
+
+        <SwipeableDrawer
+          open={this.state.open_drawer}
+          onClose={this.toggleDrawer(false)}
+          onOpen={this.toggleDrawer(true)}
+          disableBackdropTransition={!iOS}
+          disableDiscovery={iOS}
+        >
+          <div
+            tabIndex={0}
+            role="button"
+            onClick={this.toggleDrawer(false)}
+            onKeyDown={this.toggleDrawer(false)}
+          >
+            {sideList}
+          </div>
+        </SwipeableDrawer>
+      </div>
+    )
+  }
+}
+
+HomePage.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(HomePage);
+
