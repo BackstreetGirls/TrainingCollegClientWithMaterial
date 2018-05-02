a137d693d21698e584983195ede028d3c2149b57
Lili0820 <1179028267@qq.com>
2018-05-02 10:15:25 +0800

    <fix>:<router of new page>

--- src/router.js
+++ src/router.js
@@ -2,6 +2,10 @@ import React from 'react';
 import {Router, Route, Switch} from 'dva/router';
 import HomePage from './routes/HomePage';
 import Page404 from './routes/404Page';
+import PlaceOrderMainPage from './routes/placeorder/PlaceOrderMainPage'
+import TraineeInfoPage from './routes/traineeinfo/TraineeInfoPage'
+import ChangePasswordPage from './routes/traineeinfo/ChangePasswordPage'
+
 
 function RouterConfig({history}) {
   return (
@@ -15,6 +19,10 @@ function RouterConfig({history}) {
             {/*</Switch>*/}
           {/*</NavigationPage>*/}
         {/*</Route>*/}
+        <Route path='/placeorder' exact component={PlaceOrderMainPage}/>
+        <Route path='/traineecenter' exact component={TraineeInfoPage}/>
+        <Route path='/changepassword' exact component={ChangePasswordPage}/>
+
         <Route path='*' exact component={Page404}/>
       </Switch>
     </Router>
