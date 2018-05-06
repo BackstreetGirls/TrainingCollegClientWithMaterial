f20d9c9218710d7a223f1a92a7d6c0d1342cae9a
Lili0820 <1179028267@qq.com>
2018-05-06 23:07:03 +0800

    <fix>:<TraineeInfoPage of material>

--- src/routes/traineeinfo/TraineeInfoPage.js
+++ src/routes/traineeinfo/TraineeInfoPage.js
@@ -3,170 +3,72 @@
  */
 import React from 'react';
 import {connect} from 'dva';
-import {Card, Icon, Modal, Form, Input, Button} from 'antd';
+
+import Dialog, {
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from 'material-ui/Dialog';
+import Button from 'material-ui/Button';
+import TextField from 'material-ui/TextField';
+
+import {Card, Icon, Modal, Form, Input} from 'antd';
 import styles from '../css/TraineeInfoPage.css';
 import Navigation from '../../components/Navigation';
-import {Avatar } from 'antd';
+import {Avatar} from 'antd';
 import avatar from '../../assets/avatar.png';
-const { Meta } = Card;
+const {Meta} = Card;
 
-
-const FormItem = Form.Item;
-
-function level_info() {
-  Modal.info({
-    title: '会员等级规则',
-    content: (
-      <div>
-        <p>
-          累计消费满100元 --1级<br/>
-          累计消费满200元 --2级<br/>
-          累计消费满500元 --3级<br/>
-          累计消费满1000元 --4级<br/>
-          累计消费满2000元 --5级<br/>
-          累计消费满5000元 --6级<br/>
-          累计消费满10000元 --7级<br/>
-          累计消费满20000元 --8级<br/>
-          累计消费满50000元 --9级<br/>
-          累计消费满100000元 --10级
-        </p>
-      </div>
-    ),
-    onOk() {
-    },
-    okText: "I know",
-  });
-}
-
-function discount_info() {
-  Modal.info({
-    title: 'Discount Rule',
-    content: (
-      <div>
-        <p>
-          累计消费满100元 --0.99折<br/>
-          累计消费满200元 --0.98折<br/>
-          累计消费满500元 --0.95折<br/>
-          累计消费满1000元 --0.90折<br/>
-          累计消费满2000元 --0.88折<br/>
-          累计消费满5000元 --0.85折<br/>
-          累计消费满10000元 --0.80折<br/>
-          累计消费满20000元 --0.77折<br/>
-          累计消费满50000元 --0.75折<br/>
-          累计消费满100000元 --0.70折
-        </p>
-      </div>
-    ),
-    onOk() {
-    },
-    okText: "我知道了",
-  });
-}
-
-function credit_info() {
-  Modal.info({
-    title: '会员积分规则',
-    content: (
-      <div>
-        <p>
-          单笔消费每满100元，可获得5点积分。<br/>
-          1点积分可在购买课程时可抵扣1元金额。
-        </p>
-      </div>
-    ),
-    onOk() {
-    },
-    okText: "我知道了",
-  });
-}
-
-const CreditsExchangeForm = Form.create()(
-  (props) => {
-    const {visible, onCancel, onCreate, form} = props;
-    const {getFieldDecorator} = form;
-    const formItemLayout = {
-      labelCol: {
-        xs: {span: 24},
-        sm: {span: 4},
-      },
-      wrapperCol: {
-        xs: {span: 24},
-        sm: {span: 16},
-      },
-    };
-
-    return (
-      <Modal
-        visible={visible}
-        title="积分兑换"
-        okText="兑换"
-        cancelText="取消"
-        onCancel={onCancel}
-        onOk={onCreate}
-      >
-        <Form layout="vertical" style={{width: '100%', marginTop: 20, marginLeft: 30}}>
-          <FormItem {...formItemLayout} label="兑换积分">
-            {getFieldDecorator('credits', {
-              rules: [{required: true, message: '请输入您要兑换的积分数额！'}],
-            })(
-              <Input/>
-            )}
-          </FormItem>
-          <FormItem {...formItemLayout} label="银行卡号">
-            {getFieldDecorator('identity', {
-              rules: [{required: true, message: '请输入您的积分兑换入的银行卡号！'}],
-            })(
-              <Input/>
-            )}
-          </FormItem>
-        </Form>
-      </Modal>
-    );
-  }
-);
 
 class TraineeInfoPage extends React.Component {
 
 
   state = {
-    visible: false,
+    open: false,
+    level:false,
+    discount:false,
+    credit:false,
   };
 
-  showCreditsExchangeModal = () => {
-    this.setState({visible: true});
+  //兑换对话框
+  handleClickOpen = () => {
+    this.setState({open: true});
   };
 
-  handleCancel = () => {
-    this.setState({visible: false});
+  handleClose = () => {
+    this.setState({open: false});
+    // 1s后刷新本页面
+    this.timer = setInterval(() => {
+      window.location.reload(true);
+    }, 1000);
   };
 
-  handleCreate = () => {
-    const form = this.form;
-    form.validateFields((err, values) => {
-      if (err) {
-        return;
-      }
-      const param = {
-        ...values,
-        trainee_id: this.props.trainee.trainee_id,
-      };
-      this.props.dispatch({
-        type: 'trainee/creditsExchange',
-        payload: {
-          ...param,
-        },
-      });
-      form.resetFields();
-      this.setState({visible: false});
-      // 1s后刷新本页面
-      this.timer = setInterval(() => {
-        window.location.reload(true);
-      }, 1000);
-    });
+  //等级规则
+  level_info_close=()=> {
+    this.setState({level: false});
   };
 
-  saveFormRef = (form) => {
-    this.form = form;
+  level_info_open=()=>{
+    this.setState({level: true});
+  };
+
+  //优惠规则
+  discount_info_close=()=> {
+    this.setState({discount: false});
+  };
+
+  discount_info_open=()=>{
+    this.setState({discount: true});
+  };
+
+  //积分规则
+  credit_info_close=()=> {
+    this.setState({credit: false});
+  };
+
+  credit_info_open=()=>{
+    this.setState({credit: true});
   };
 
   render() {
@@ -176,27 +78,27 @@ class TraineeInfoPage extends React.Component {
         <div className={styles.inner_wrapper}>
           <div className={styles.welcome}>
             <div>
-              <Avatar src={avatar} style={{width:100,height:100}}/>
+              <Avatar src={avatar} style={{width: 100, height: 100}}/>
 
             </div>
           </div>
           <Card
-            title="我的会员信息"
+            title="Vip Info"
             className={styles.card}
             // cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
             actions={[
-              <span onClick={level_info}>
+              <Button color="primary" size="small" onClick={(index)=>{this.level_info_open(index)}}>
                 <Icon type="question-circle-o" style={{marginRight: 2}}/>
-                查看会员等级规则
-              </span>,
-              <span onClick={discount_info}>
+                Vip Level Rule
+              </Button>,
+              <Button color="primary" size="small" onClick={(index)=>{this.discount_info_open(index)}}>
                 <Icon type="question-circle-o" style={{marginRight: 2}}/>
-                查看折扣优惠规则
-              </span>,
-              <span onClick={credit_info}>
+                Discount Rule
+              </Button>,
+              <Button color="primary" size="small" onClick={(index)=>{this.credit_info_open(index)}}>
                 <Icon type="question-circle-o" style={{marginRight: 2}}/>
-                查看会员积分规则
-              </span>
+                Vip BP Rule
+              </Button>
             ]}
             extra={
               <div>
@@ -208,29 +110,155 @@ class TraineeInfoPage extends React.Component {
             }
           >
             <Meta
-              title="累计消费(¥)"
-              description={3435 + "元"}
+              title="Consumption(¥)"
+              description={3435}
             /><br/>
             <Meta
-              title="会员等级"
-              description={5 + "级"}
+              title="Vip Level"
+              description={5}
             /><br/>
             <Meta
-              title="可享折扣优惠"
-              description={0.88 === 1 ? "暂无可使用优惠折扣" : 0.88 + "折"}
+              title="Discount(% off)"
+              description={0.88 === 1 ? "No discount" : "12"}
             /><br/>
             <Meta
-              title="会员积分"
-              description={2000 + "分"}
+              title="BP(point)"
+              description={2000}
             />
-            <Button onClick={this.showCreditsExchangeModal} style={{marginLeft:'80%'}}>积分兑换</Button>
+            <Button color="primary" variant="raised" onClick={this.handleClickOpen}
+                    style={{marginLeft: '80%'}}>EXCHANGE</Button>
           </Card>
-          <CreditsExchangeForm
-            ref={this.saveFormRef}
-            visible={this.state.visible}
-            onCancel={this.handleCancel}
-            onCreate={this.handleCreate}
-          />
+
+          <Dialog
+            open={this.state.open}
+            onClose={this.handleClose}
+            aria-labelledby="form-dialog-title"
+          >
+            <DialogTitle id="form-dialog-title">Exchange BP</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                To exchange more BP, please enter the amount of the BP you want to exchange and the bankID of your pay.
+              </DialogContentText>
+              <div style={{marginLeft:"33%"}}>
+                <TextField
+                  required
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  id="num"
+                  label="Amount"
+                  type="number"
+                  placeholder="1000"
+                  margin="normal"
+                />
+                <br/>
+                <TextField
+                  autoFocus
+                  required
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  margin="normal"
+                  id="name"
+                  label="BankId"
+                  placeholder="622888888123456789"
+                  type="email"
+                />
+              </div>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={this.handleCancel} color="primary">
+                Cancel
+              </Button>
+              <Button onClick={this.handleClose} color="primary">
+                Exchange
+              </Button>
+            </DialogActions>
+          </Dialog>
+
+          <Dialog
+            open={this.state.level}
+            onClose={(index)=>{this.level_info_close(index)}}
+            aria-labelledby="form-dialog-title"
+          >
+            <DialogTitle id="form-dialog-title1">Vip Level Rule</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                The higher the amount of consumption, the higher the membership grade.
+              </DialogContentText>
+              <div style={{marginLeft:"23%"}}>
+                <p>
+                  Total consumption reached ¥100   --level 1<br/>
+                  Total consumption reached ¥200   --level 2<br/>
+                  Total consumption reached ¥500   --level 3<br/>
+                  Total consumption reached ¥1000  --level 4<br/>
+                  Total consumption reached ¥2000  --level 5<br/>
+                  Total consumption reached ¥5000  --level 6<br/>
+                  Total consumption reached ¥10000 --level 7<br/>
+                  Total consumption reached ¥20000 --level 8<br/>
+                  Total consumption reached ¥50000 --level 9<br/>
+                  Total consumption reached ¥10000 --level 10
+                </p>
+              </div>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={(index)=>{this.level_info_close(index)}} color="primary">
+                I KNOW
+              </Button>
+            </DialogActions>
+          </Dialog>
+
+          <Dialog
+            open={this.state.discount}
+            onClose={(index)=>{this.discount_info_close(index)}}
+            aria-labelledby="form-dialog-title"
+          >
+            <DialogTitle id="form-dialog-title2">Discount Rule</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                According to different cumulative consumption, give different discount. The higher the amount of consumption, the greater the discounts.
+              </DialogContentText>
+              <div style={{marginLeft:"23%"}}>
+                <p>
+                  Total consumption reached ¥100   --1% off<br/>
+                  Total consumption reached ¥200   --2% off<br/>
+                  Total consumption reached ¥500   --5% off<br/>
+                  Total consumption reached ¥1000  --10% off<br/>
+                  Total consumption reached ¥2000  --12% off<br/>
+                  Total consumption reached ¥5000  --15% off<br/>
+                  Total consumption reached ¥10000 --20% off<br/>
+                  Total consumption reached ¥20000 --23% off<br/>
+                  Total consumption reached ¥50000 --25% off<br/>
+                  Total consumption reached ¥10000 --30% off
+                </p>
+              </div>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={(index)=>{this.discount_info_close(index)}} color="primary">
+                I KNOW
+              </Button>
+            </DialogActions>
+          </Dialog>
+
+          <Dialog
+            open={this.state.credit}
+            onClose={(index)=>{this.credit_info_close(index)}}
+            aria-labelledby="form-dialog-title"
+          >
+            <DialogTitle id="form-dialog-title3">VIP BP Rule</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                A single consumption of 100 yuan, can get 5 points.<br/>
+                1 points can deduct 1 yuan in the course of purchase.
+              </DialogContentText>
+
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={(index)=>{this.credit_info_close(index)}} color="primary">
+                I KNOW
+              </Button>
+            </DialogActions>
+          </Dialog>
         </div>
       </div>
     )
