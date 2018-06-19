3e68ce4f055e95d7202b2c5ce982ed8293d5c025
Lili0820 <1179028267@qq.com>
2018-06-19 10:07:22 +0800

    <fix> : <the name of page>

--- src/models/Supervisor.js
+++ src/models/Supervisor.js
@@ -151,6 +151,15 @@ export default {
         else if(pathToRegexp('/changepassword').exec(location.pathname)){
           document.title = '修改密码';
         }
+        else if(pathToRegexp('/detail&courseID=:0').exec(location.pathname)){
+          document.title='课程详情';
+        }
+        else if(pathToRegexp('/allcourses').exec(location.pathname)){
+          document.title='全部课程';
+        }
+        else if(pathToRegexp('/searchresult').exec(location.pathname)){
+          document.title='搜索结果';
+        }
         else {
           document.title = '页面不存在';
         }
